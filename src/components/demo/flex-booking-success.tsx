5e9d2377fc0a1dbd660d10750ae5caffd27c9a52
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2026-09-21 15:54:01 +0700

    update disable เปิด liff อีกครั้ง

--- src/components/demo/flex-booking-success.tsx
+++ src/components/demo/flex-booking-success.tsx
@@ -65,9 +65,9 @@ export function FlexBookingSuccess({
         <Button onClick={() => onAction?.('cancel')} fullWidth variant="contained" sx={{ bgcolor: '#dfe3e9', color: '#2b3440', boxShadow: 'none' }}>
           ยกเลิกคิว
         </Button>
-        <Button onClick={() => onAction?.('open_liff')} fullWidth variant="contained" sx={{ bgcolor: '#12a862', color: '#fff', boxShadow: 'none', '&:hover': { bgcolor: '#5ead77' } }}>
+      {/*   <Button onClick={() => onAction?.('open_liff')} fullWidth variant="contained" sx={{ bgcolor: '#12a862', color: '#fff', boxShadow: 'none', '&:hover': { bgcolor: '#5ead77' } }}>
           เปิด LIFF อีกครั้ง
-        </Button>
+        </Button> */}
       </Stack>
     </Box>
   );
